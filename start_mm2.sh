c12a05d67cad312227e8ff54bdb35aab23862793
smk762 <smk762@iinet.net.au>
2023-05-14 11:14:30 +0000

    use nohup

--- start_mm2.sh
+++ start_mm2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source rpc
-stdbuf -oL ./mm2 > mm2.log &
+
+nohup ./mm2 > mm2.log &
 sleep 3
 curl --url "http://${rpcip}:${rpcport}" --data "{\"method\":\"version\",\"userpass\":\"$userpass\"}"
